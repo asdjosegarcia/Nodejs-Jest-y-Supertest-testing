82372327d94c27bafffb147065307f6b452831ce
asdjosegarcia <asdjosegarcia@gmail.com>
2024-04-27 23:24:32 -0300

    completed

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
 import express from 'express'//import express
+import { v4 } from 'uuid'
+
+const tasks=[];
 
 const app=express()//a new instance from express 
+app.use(express.json())//any request that comes transform to json
+
 
 app.get('/ping',(req,res)=>{ //when a ping request arrives, /ping is a route from request
     res.send("pong")//we return a "pong" text
 })
 
 app.get('/tasks',(req,res)=>{
-    res.json([]);
+    res.status(200).json(tasks);
 })
 
 app.post('/tasks',(req,res)=>{
-    res.json({})//we send a json response
+    const {title,description}= req.body;
+
+    if(!title || !description) return res.sendStatus(400)//if ther no title and description, send status 400
+
+    res.json({//we send a json response
+            title,
+            description,
+            id:v4()//this id is going to come from uuid
+    })
 })
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -15,21 +15,50 @@ describe('GET /tasks', () => {//we check if there is a GET /tasks path
 })
 
 describe('POST /tasks', () => {
-    //this should respond with a status code 200
-    test('should respond with a 200 status ', async () => {
-        const response = await request(app).post('/tasks').send();
-        expect(response.statusCode).toBe(200)
+    describe('given a title and description',()=>{
+        const newTask={
+            title:"Somte title",
+            description:"dome description"
+        }
+
+        test('should respond with a 200 status ', async () => {
+            const response = await request(app).post('/tasks').send(newTask);
+            expect(response.statusCode).toBe(200)
+        })
+    
+        //this should respond with a content-type of aplication/json
+        test('should have a content-type: application/json in the header', async () => {
+            const response = await request(app).post("/tasks").send(newTask);
+            expect(response.header["content-type"]).toEqual(
+                expect.stringContaining("json") //we ask if the head has the json text
+            )
+        })
+    
+        //should respond with a json object containing the new task with an id
+        test('should respond with an task ID',async()=>{
+            const response=await request(app).post("/tasks").send(newTask)
+            console.log(response.body);
+            expect(response.body.id).toBeDefined();//if id is defined, as text, number, etc etc...
+        })
+    
     })
 
-    //this should respond with a content-type of aplication/json
-    test('should have a content-type: application/json in the header', async () => {
-        const response = await request(app).post("/tasks");
-        expect(response.header["content-type"]).toEqual(
-            expect.stringContaining("json")
-        )
+    describe('when title and description is missing',()=>{
+        test('should respond with a 400 status code',async ()=>{
+            const fields=[//send a multiples object to multiple tests in a only test
+            {},
+            {title:'Test Task'},
+            {description:'Test Description'},
+            ]
+
+            for(const body of fields){
+                const response=await request(app).post('/tasks').send(body)
+                expect(response.statusCode).toBe(400)
+            }
+        })
+
     })
 
-    //should respondo with a json object containing the new task with an id
 
 })
 
